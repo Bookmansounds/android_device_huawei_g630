6b2238403c9527db8ab9fce470d511022ffb0711
EloYGomeZ <eloygomez83@gmail.com>
2014-12-08 14:16:49 +0100

    reconfigure wifi device3

--- wifi/Android.mk
+++ wifi/Android.mk
@@ -45,8 +45,17 @@ LOCAL_SRC_FILES    := $(LOCAL_MODULE)
 LOCAL_MODULE_PATH  := $(TARGET_OUT_ETC)/wifi
 include $(BUILD_PREBUILT)
 
+
 include $(CLEAR_VARS)
-LOCAL_MODULE := WCNSS_qcom_wlan_nv.bin
+LOCAL_MODULE       := WCNSS_qcom_wlan_nv.bin
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := $(LOCAL_MODULE)
+LOCAL_MODULE_PATH  := $(TARGET_OUT_ETC)/wifi
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := WCNSS_hw_wlan_nv.bin.bin
 LOCAL_MODULE_CLASS := FAKE
 LOCAL_MODULE_TAGS := optional
 include $(BUILD_SYSTEM)/base_rules.mk
